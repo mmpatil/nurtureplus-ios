1e97a1b97549a9bc420e30b52250b8beb4da5a90
Manali Patil <mmpatil34@gmail.com>
2026-03-02 00:05:59 -0800

    Initial Commit

---
+++ nurtureplus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  nurtureplus
+//
+//  Created by Manali Maruti Pat on 3/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ nurtureplusTests/nurtureplusTests.swift
@@ -0,0 +1,16 @@
+//
+//  nurtureplusTests.swift
+//  nurtureplusTests
+//
+//  Created by Manali Maruti Pat on 3/2/26.
+//
+
+import Testing
+
+struct nurtureplusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
